962c73fca7f769881bc24856434979d3e0685766
PailBoy <pailboyer@gmail.com>
2024-12-12 10:56:59 +0100

    Create adresse.h

---
+++ adresse.h
@@ -0,0 +1,15 @@
+//
+// Created by Jeen Yuhs on 12/12/2024.
+//
+
+#ifndef ADRESSE_H
+#define ADRESSE_H
+
+
+extern const char *node;
+extern const char *service;
+
+void adresse_init() ;
+
+
+#endif //ADRESSE_H
