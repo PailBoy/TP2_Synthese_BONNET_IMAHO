d381d34bd1908b163e6ab173320d30459fbe5241
PailBoy <pailboyer@gmail.com>
2024-12-19 08:22:37 +0100

    Update sockets_utils.c

--- sockets_utils.c
+++ sockets_utils.c
@@ -1,45 +1,27 @@
-#include "adresse.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
+#include "sockets_utils.h"
 
-// Définition des variables globales
-const char *node = "localhost"; // Chercher automatiquement localhost
-const char *service = "69";     // Port TFTP (69)
 
-char adresse_init() {
-    struct addrinfo hints, *res, *p;
-
-    // Initialisation de la structure hints
+struct addrinfo *get_server_address(const char *node, const char *service) {
+    struct addrinfo hints, *res;
     memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_INET; // Utiliser IPv4 uniquement
-    hints.ai_socktype = SOCK_DGRAM; // TFTP utilise UDP
-    hints.ai_flags = 0; // Ne pas utiliser AI_PASSIVE
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_DGRAM; // UDP
     hints.ai_protocol = IPPROTO_UDP;
 
     int status = getaddrinfo(node, service, &hints, &res);
-    struct addrinfo *current = res;
 
-    int fd = socket(current->ai_family, current->ai_socktype, current->ai_protocol);
-    connect(fd, (struct sockaddr*)res, TRAME_LENGTH)
+    return res;
+}
 
-    printf("Résultats pour le serveur TFTP local :\n");
-    for (p = res; p != NULL; p = p->ai_next) {
-        char addr_str[INET_ADDRSTRLEN];
-        struct sockaddr_in *addr = (struct sockaddr_in *)p->ai_addr;
-        inet_ntop(AF_INET, &addr->sin_addr, addr_str, sizeof(addr_str));
-        printf("Adresse : %s\n", addr_str);
-        return (char) addr_str;
+int create_udp_socket(struct addrinfo *res) {
+    int fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    return fd;
+}
 
+ssize_t send_udp_message(int sockfd, const void *buffer, size_t len, struct addrinfo *res) {
 
-    }
+        ssize_t bytes_sent = sendto(sockfd, buffer, len, 0, res->ai_addr, res->ai_addrlen);
 
-    // Libération de la mémoire
-    freeaddrinfo(res);
+        return bytes_sent;
 }
 
