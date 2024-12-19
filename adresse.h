0e76b300e6f5f61cbb5652e12fd56a9de4e3aff9
PailBoy <pailboyer@gmail.com>
2024-12-19 08:20:45 +0100

    Update adresse.h

--- adresse.h
+++ adresse.h
@@ -1,15 +1,18 @@
-//
-// Created by Jeen Yuhs on 12/12/2024.
-//
-
 #ifndef ADRESSE_H
 #define ADRESSE_H
 
+#include "build_rrq_request.h"
+#include "sockets_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stddef.h>
 
-extern const char *node;
-extern const char *service;
+#define TRAME_LENGTH 512
 
-void adresse_init() ;
-
+void adresse_init();
+size_t build_rrq_request(char *buffer, const char *filename, const char *mode);
 
 #endif //ADRESSE_H
